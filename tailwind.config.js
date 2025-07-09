90ff3d307e000bdc44175b5f336dfa0d6e6ea648
Boaz Marube <boazmarube1@gmail.com>
2025-07-09 14:32:32 +0300

    Initial commit: Basic setup

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "class",
+  content: ["./index.html","./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        customBrown: "#8C3D24",
+        "ethiopian-red": "#8c3a24",
+        "ethiopian-dark": "#26100A",
+        menu: "#FFF1C6",
+        "menu-title": "#4A3A0C",
+        "contact-bg": "#93532A",
+        "dish-des": "#4A3A0C",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        playfair: ['"Playfair Display"', "serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+
