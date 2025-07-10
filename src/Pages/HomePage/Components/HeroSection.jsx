7c81883fa57ab2cfdf600e67f85b168f2faeb1ab
Bereket-Eshete <bereketeshete63@gmail.com>
2025-07-10 18:47:28 +0300

    fix images path and add logo images in the header component

--- src/Pages/HomePage/Components/HeroSection.jsx
+++ src/Pages/HomePage/Components/HeroSection.jsx
@@ -1,8 +1,8 @@
-import img1 from "../assets/get-togethers.jpg";
-import img2 from "../assets/meeting-1.jpg";
-import img3 from "../assets/Raw meat.jpg";
-import img4 from "../assets/Totot kitfo.jpg";
-import ImageSlider from "./ImageSlider";
+import img1 from "../../../assets/get-togethers.jpg";
+import img2 from "../../../assets/meeting-1.jpg";
+import img3 from "../../../assets/Raw meat.jpg";
+import img4 from "../../../assets/Totot kitfo.jpg";
+import ImageSlider from "../../../components/ImageSlider";
 
 const Hero = () => {
   const Images = [img1, img2, img3, img4];
